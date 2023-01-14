578d88491c0800da71d580d743487af658b5a0f7
0Magenta0 <0_magenta_0@mail.ru>
2023-01-14 21:28:37 +0300

    Upd: install.sh add fonts installation

--- install.sh
+++ install.sh
@@ -9,6 +9,16 @@
 COUNT_SUCCESSFUL=0
 COUNT_FAILED=0
 
+function check_return() {
+    if [ $? -eq 0 ]; then
+        echo -e "\x1B[32mOK\x1B[0m"
+        ((COUNT_SUCCESSFUL++))
+    else
+        echo -e "\x1B[31mFAIL ($?)\x1B[0m"
+        ((COUNT_FAILED++))
+    fi
+}
+
 # copy_config (src, dst, is_root)
 function copy_config() {
     echo -en "[\x1B[36m*\x1B[0m] Copying $1 to $2... "
@@ -20,21 +30,19 @@ function copy_config() {
         cp "$1" "$2" &>/dev/null
     fi
 
-    if [ $? -eq 0 ]; then
-        echo -e "\x1B[32mOK\x1B[0m"
-        ((COUNT_SUCCESSFUL++))
-    else
-        echo -e "\x1B[31mFAIL ($?)\x1B[0m"
-        ((COUNT_FAILED++))
-    fi
+    check_return
 }
 
 echo -e "[\x1B[36m*\x1B[0m] Getting enough privileges..."
 echo -e "[\x1B[32m+\x1B[0m] Now we can use `sudo whoami`!"
 
 echo -e "\n----========----\n"
-echo -e "[\x1B[36m*\x1B[0m] Setting timezone to Europe/Moscow..."
+echo -en "[\x1B[36m*\x1B[0m] Setting timezone to Europe/Moscow... "
 sudo ln -sf /usr/share/zoneinfo/Europe/Moscow /etc/localtime
+check_return
+echo -en "[\x1B[36m*\x1B[0m] Installing some fonts... "
+sudo pacman -S --noconfirm ttf-hack-nerd ttf-droid ttf-dejavu &>/dev/null
+check_return
 
 echo -e "\n----========----\n"
 echo -e "[\x1B[36m*\x1B[0m] Installing all configs..."
@@ -47,5 +55,5 @@ copy_config "bashrc" "$HOME/.bashrc" 0
 copy_config "bashrc" "`sudo bash -c 'echo ~'`/.bashrc" 1
 
 echo -e "\n----========----\n"
-echo -e "[\x1B[32m+\x1B[0m] Installation is ended! Success: \x1B[32m$COUNT_SUCCESSFUL\x1B[0m | Failed: \x1B[31m$COUNT_FAILED\x1B[0m"
+echo -e "[\x1B[32m+\x1B[0m] Setup is ended! Success: \x1B[32m$COUNT_SUCCESSFUL\x1B[0m | Failed: \x1B[31m$COUNT_FAILED\x1B[0m"
 
