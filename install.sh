f6d7e72d11316098b908b8520f518983c90b3ec0
0Magenta0 <0_magenta_0@mail.ru>
2023-01-14 20:30:00 +0300

    Reformat: install.sh change sudo successful message format

--- install.sh
+++ install.sh
@@ -30,7 +30,7 @@ function copy_config() {
 }
 
 echo -e "[\x1B[36m*\x1B[0m] Getting enough privileges..."
-echo -e "[\x1B[32m+\x1B[0m] Now we: `sudo whoami`!"
+echo -e "[\x1B[32m+\x1B[0m] Now we can use `sudo whoami`!"
 
 echo -e "[\x1B[36m*\x1B[0m] Installing all configs..."
 copy_config "tmux.conf" "/etc/tmux.conf" 1
